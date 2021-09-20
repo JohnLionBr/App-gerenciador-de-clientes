fe0ecd53aa490f5112ebf9158055ef9477bfd40e
João Pedro <89528455+JohnLionBr@users.noreply.github.com>
2021-09-19 22:10:35 -0300

    Começando app para gerenciar clientes, em React.
    Faltando pasta node_modules!! Terminar depois.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import Titulo from "./Titulo"
+
+interface LayoutProps{
+    titulo: string
+    children: any
+}
+
+export default function Layout(props: LayoutProps) {
+return (
+    <div className={`
+        flex flex-col w-2/3 bg-white
+        text-black rounded-md
+    `}>
+        <Titulo>
+            {props.titulo}
+        </Titulo>
+        <div className="p-6">
+            {props.children}
+
+        </div>
+
+
+
+    </div>
+
+    
+)
+}
