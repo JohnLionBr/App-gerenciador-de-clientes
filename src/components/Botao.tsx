7ceea6b006bbe8b33f9c77518baa3523d293a3e3
João Pedro <89528455+JohnLionBr@users.noreply.github.com>
2021-09-21 21:40:17 -0300

    Adicionando componente botao

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -1,11 +1,17 @@
 interface BotaoProps{
+    className: string
     children: any
-    cor?: `green` | `blue` | `gray`
+    cor?: 'green' | 'blue' | 'gray'
 }
 
 export default function Botao(props: BotaoProps) {
+    const cor = props.cor ?? 'gray'
     return(
-        <button>
+        <button className={`
+            bg-gradient-to-r from-${cor}-400 to-${cor}-700
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}>
             {props.children}
         </button>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import Botao from "../components/Botao";
 
 export default function Home() {
 
@@ -29,6 +30,9 @@ export default function Home() {
     text-white
     `}>
      <Layout titulo="Cadastro simples">
+       <div className="flex justify-end">
+         <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+       </div>
        <Tabela clientes={clientes}
          clienteSelecionado={clienteSelecionado}
          clienteExcluido={clienteExcluido}>
